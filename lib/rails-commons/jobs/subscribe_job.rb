f1327f1bf1683e164d253df8d688b0dcaffadb7f
Pawel Niewiadomski <11110000b@gmail.com>
2016-11-04 07:18:30 +0100

    put SubscribeJob into a module

--- lib/rails-commons/jobs/subscribe_job.rb
+++ lib/rails-commons/jobs/subscribe_job.rb
@@ -1,18 +1,22 @@
-class SubscribeJob < ApplicationJob
-  def perform(email, merge_fields = {})
-    lower_case_md5_hashed_email_address = Digest::MD5.hexdigest(email.downcase)
+module RailsCommons
 
-    gb = Gibbon::Request.new
-    gb.lists(Rails.application.secrets.mailchimp_list_id).members(lower_case_md5_hashed_email_address).upsert(
-      body: {
-        email_address: email,
-        status_if_new: 'subscribed',
-        merge_fields: merge_fields || {},
-        interests: interests
-      })
+  class SubscribeJob < ApplicationJob
+    def perform(email, merge_fields = {})
+      lower_case_md5_hashed_email_address = Digest::MD5.hexdigest(email.downcase)
+
+      gb = Gibbon::Request.new
+      gb.lists(Rails.application.secrets.mailchimp_list_id).members(lower_case_md5_hashed_email_address).upsert(
+        body: {
+          email_address: email,
+          status_if_new: 'subscribed',
+          merge_fields: merge_fields || {},
+          interests: interests
+        })
+    end
+
+    def interests
+      Hash[(Rails.application.secrets.mailchimp_interest_ids || []).map { |_| [_, true] }]
+    end
   end
 
-  def interests
-    Hash[(Rails.application.secrets.mailchimp_interest_ids || []).map { |_| [_, true]}]
-  end
 end
